ba24292ccef5b107bca476fe03c733154d4f9e37
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2019-12-11 13:46:35 +0100

    added class activity

--- src/main/java/com/wemakeitwork/allenvooreen/model/Activity.java
+++ src/main/java/com/wemakeitwork/allenvooreen/model/Activity.java
@@ -9,5 +9,34 @@ public class Activity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer ActivityId;
+    private Integer activityId;
+
+    private String activityName;
+
+    private String activityCategory;
+
+    public Integer getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Integer activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public String getActivityCategory() {
+        return activityCategory;
+    }
+
+    public void setActivityCategory(String activityCategory) {
+        this.activityCategory = activityCategory;
+    }
+
 }
