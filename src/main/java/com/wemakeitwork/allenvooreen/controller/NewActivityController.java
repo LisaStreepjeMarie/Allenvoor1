2f2f0dbf15ff3758be2c124d72b9005a456bece7
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2019-12-11 14:25:13 +0100

    added save getmapping activity on activitycontroller

--- src/main/java/com/wemakeitwork/allenvooreen/controller/NewActivityController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/NewActivityController.java
@@ -1,6 +1,21 @@
 package com.wemakeitwork.allenvooreen.controller;
+import com.wemakeitwork.allenvooreen.model.Activity;
+import com.wemakeitwork.allenvooreen.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NewActivityController {
+
+    @Autowired
+    ActivityRepository activityRepository;
+
+    @GetMapping("/newActivity")
+    String doorsturen(Activity activity){
+        activityRepository.save(activity);
+
+        //TODO direct back to the event page it came from
+        return "redirect:/...";
+    }
 }
