96f15972b2689f0d9a8bada6a5da493cc1cbe6ec
Anna-Marije Procee-Brand <ambrand5@hotmail.com>
2019-12-12 16:14:27 +0100

    Created delete member controller (not functional yet because of missing login)

--- src/main/java/com/wemakeitwork/allenvooreen/controller/DeleteMemberController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/DeleteMemberController.java
@@ -1,10 +1,29 @@
 package com.wemakeitwork.allenvooreen.controller;
+import com.wemakeitwork.allenvooreen.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DeleteMemberController {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    // Verwijder gebruiker (ingelogd gebruiker kan zichzelf verwijderen)
+    @RequestMapping("/member/delete/member")
+    @ResponseBody
+    public String deleteMember(int memberId) {
+        try {
+            memberRepository.delete(memberRepository.getOne(memberId));
+        } catch (Exception ex) {
+            return "Error bij het verwijderen van gebruiker:" + ex.toString();
+        }
+        return "Gebruiker verwijderd!";
+    }
 }
+
 
 
 
