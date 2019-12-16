3e549b5517bf87be722fee76de72745c8d6fb940
Luisella <luisella@xs4all.nl>
2019-12-16 10:25:07 +0100

    class NewTeamController

--- src/main/java/com/wemakeitwork/allenvooreen/controller/NewTeamController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/NewTeamController.java
@@ -1,6 +1,28 @@
 package com.wemakeitwork.allenvooreen.controller;
+import com.wemakeitwork.allenvooreen.model.Team;
+import com.wemakeitwork.allenvooreen.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NewTeamController {
+
+    @Autowired
+    TeamRepository teamRepository;
+
+    @GetMapping("/teams")
+    protected String showTeams(Model model){
+        model.addAttribute("allTeams", teamRepository.findAll());
+        return "teamOverview";
+    }
+
+    @GetMapping("/teams/add")
+    protected String showTeamForm(Model model){
+        model.addAttribute("team", new Team());
+        model.addAttribute("teamList", teamRepository.findAll());
+        return "teamForm";
+    }
+
 }
