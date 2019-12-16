5bcb241e1cbe3d3f00e52dd13e552dc631e46b91
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2019-12-16 10:20:37 +0100

    Postmapping works for activity controller

--- src/main/java/com/wemakeitwork/allenvooreen/controller/NewActivityController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/NewActivityController.java
@@ -3,8 +3,11 @@ import com.wemakeitwork.allenvooreen.model.Activity;
 import com.wemakeitwork.allenvooreen.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -14,19 +17,24 @@ public class NewActivityController {
     ActivityRepository activityRepository;
 
     //TODO in this sprint we don't need this form. Remember to link this back to the event
+
+
     @GetMapping("/activity/new")
-    String newActivity(){
+    protected String showActivityForm(Model model) {
+        model.addAttribute("activity", new Activity());
         return "newActivity";
     }
 
-    @GetMapping("/activity/save")
-    protected String saveOrUpdateActivity(Activity activity, BindingResult result){
+    @PostMapping("/activity/new")
+    protected String saveOrUpdateActivity(@ModelAttribute("activity") Activity activity, BindingResult result) {
         if (result.hasErrors()) {
             return "newActivity";
-        } else {
+        }
+        else {
+            System.out.println("het werkt!");
+            activity.setActivityId(activity.getActivityId());
             activityRepository.save(activity);
             return "redirect:/activity/new";
         }
     }
-
 }
--- src/main/webapp/WEB-INF/jsp/newActivity.jsp
+++ src/main/webapp/WEB-INF/jsp/newActivity.jsp
@@ -1,3 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +11,7 @@
 <h1>Voer de activiteit in!</h1>
 <br>
 
-<form action="/activity/save" modelAttribute="activity">
+<form action="/activity/new" modelAttribute="activity" method="Post">
     <input type="text" name="activityName" value="${updateActivityName}"><br><br>
         <select name="activityCategory">
             <option value="Huishouden" name="activityCategory">Huishouden</option>
