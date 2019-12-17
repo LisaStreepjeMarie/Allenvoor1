4fdabb713018e88482a2c6b4ef7b6fda57f38771
Anna-Marije Procee-Brand <ambrand5@hotmail.com>
2019-12-17 11:01:48 +0100

    added manytomany annotation so member can be linked to team.

--- src/main/java/com/wemakeitwork/allenvooreen/model/Member.java
+++ src/main/java/com/wemakeitwork/allenvooreen/model/Member.java
@@ -30,6 +30,10 @@ public class Member implements UserDetails {
         return rol;
     }
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "member_team", joinColumns = @JoinColumn(name = "member_id"), inverseJoinColumns = @JoinColumn(name = "team_id"))
+
+
     public void setRol(String rol) {
         this.rol = rol;
     }
