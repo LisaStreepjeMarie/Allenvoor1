e46e01e9c8f3778f861b23f149b401af9b8208e9
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2019-12-18 14:42:42 +0100

    repository event gechecked

--- src/test/java/com/wemakeitwork/allenvooreen/repository/EventRepositoryIntegrationTest.java
+++ src/test/java/com/wemakeitwork/allenvooreen/repository/EventRepositoryIntegrationTest.java
@@ -1,6 +1,4 @@
 package com.wemakeitwork.allenvooreen.repository;
-
-import com.wemakeitwork.allenvooreen.model.Activity;
 import com.wemakeitwork.allenvooreen.model.Event;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -8,14 +6,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class EventRepositoryIntegrationTest {
+class EventRepositoryIntegrationTest {
 
     @Autowired
     private EventRepository eventRepository;
