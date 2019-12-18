6f3a8f91e32648c21a291b9eb519068bb87a803a
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2019-12-18 11:52:51 +0100

    Controller Testing done on newActivityController

---
+++ src/test/java/com/wemakeitwork/allenvooreen/controller/newActivityControllerTest.java
@@ -0,0 +1,81 @@
+package com.wemakeitwork.allenvooreen.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.wemakeitwork.allenvooreen.model.Activity;
+import com.wemakeitwork.allenvooreen.repository.ActivityRepository;
+
+
+import com.wemakeitwork.allenvooreen.service.MemberDetailsService;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = NewActivityController.class)
+
+class newActivityControllerTest {
+
+    @MockBean
+    ActivityRepository activityRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    MemberDetailsService memberDetailsService;
+
+    @Test
+    @WithMockUser(roles = "admin")
+    public void shouldReturnNewActivity() throws Exception {
+        mockMvc.perform(get("/activity/new"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/newActivity.jsp"));
+    }
+
+    @Test
+    @WithMockUser(roles = "admin")
+    public void shouldReturnNewActivityPost() throws Exception {
+        String activityname = "test1";
+        String activitycategory = "test2";
+
+        Activity testActivity = new Activity();
+        testActivity.setActivityName(activityname);
+        testActivity.setActivityCategory(activitycategory);
+        testActivity.setActivityId(2);
+
+        mockMvc.perform(post("/activity/new")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .sessionAttr("activity", testActivity)
+                .with(csrf())
+        )
+                .andExpect(redirectedUrl("/activity/new"));
+    }
+
+}
--- src/test/java/com/wemakeitwork/allenvooreen/repository/ActivityRepositoryIntegrationTest.java
+++ src/test/java/com/wemakeitwork/allenvooreen/repository/ActivityRepositoryIntegrationTest.java
@@ -6,7 +6,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
