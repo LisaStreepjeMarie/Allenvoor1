f3c8c3af50724c89379e8c9e95d7b0e8a9f9b145
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2019-12-19 09:51:47 +0100

    checkpush, code licht opgeschoond

--- src/main/java/com/wemakeitwork/allenvooreen/controller/ChangeActivityController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/ChangeActivityController.java
@@ -3,9 +3,7 @@ import com.wemakeitwork.allenvooreen.model.Activity;
 import com.wemakeitwork.allenvooreen.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
