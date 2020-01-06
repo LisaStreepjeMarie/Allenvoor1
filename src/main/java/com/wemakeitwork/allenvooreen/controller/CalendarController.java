a804af2febc4afa636d9748d40f484cc59f94529
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2020-01-06 18:04:54 +0100

    Team has a list of Events and the team controls the calendar view

--- src/main/java/com/wemakeitwork/allenvooreen/controller/CalendarController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/CalendarController.java
@@ -4,21 +4,42 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wemakeitwork.allenvooreen.model.Activity;
 import com.wemakeitwork.allenvooreen.model.Event;
+import com.wemakeitwork.allenvooreen.model.Member;
+import com.wemakeitwork.allenvooreen.model.Team;
 import com.wemakeitwork.allenvooreen.repository.EventRepository;
+import com.wemakeitwork.allenvooreen.repository.MemberRepository;
+import com.wemakeitwork.allenvooreen.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CalendarController {
     @Autowired
     EventRepository eventRepository;
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
+
     @GetMapping("/calendar")
-    public String calendar(Model model) throws JsonProcessingException {
-        List<Event> eventList = eventRepository.findAll();
+    public String calendar(Model model, Principal principal) throws JsonProcessingException {
+        List<Event> eventList = new ArrayList<>();
+        Optional<Member> member = memberRepository.findByMembername(principal.getName());
+        Team team = new Team();
+        if(member.isPresent()){
+            team = teamRepository.findTeamById(teamRepository.findTeamIdByMemberid(member.get().getMemberId()));
+            eventList = team.getEventList();
+        }
+
         String calendarData = "";
         for (Event event : eventList) {
             calendarData += "" + new ObjectMapper().writeValueAsString(event) + ",";
--- src/main/java/com/wemakeitwork/allenvooreen/repository/TeamRepository.java
+++ src/main/java/com/wemakeitwork/allenvooreen/repository/TeamRepository.java
@@ -2,6 +2,7 @@ package com.wemakeitwork.allenvooreen.repository;
 
 import com.wemakeitwork.allenvooreen.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,4 +13,10 @@ public interface TeamRepository extends JpaRepository<Team, Integer> {
 
     @Override
     void deleteById(Integer integer);
+
+    @Query(value = "SELECT team_name_team_id FROM members_team_name WHERE member_member_id = ?1", nativeQuery = true)
+    Integer findTeamIdByMemberid(Integer member_id);
+
+    @Query(value = "SELECT * FROM team WHERE team_id = ?1", nativeQuery = true)
+    Team findTeamById(Integer team_id);
 }
