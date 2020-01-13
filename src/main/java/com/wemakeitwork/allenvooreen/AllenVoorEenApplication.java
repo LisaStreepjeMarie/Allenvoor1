f21819cac02a82a0f80838db245aae9a2f5f2321
Harmen Kets <harmenkets@gmail.com>
2020-01-13 17:10:46 +0100

    Implemented war packaging

--- src/main/java/com/wemakeitwork/allenvooreen/AllenVoorEenApplication.java
+++ src/main/java/com/wemakeitwork/allenvooreen/AllenVoorEenApplication.java
@@ -2,12 +2,14 @@ package com.wemakeitwork.allenvooreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 @SpringBootApplication
-public class AllenVoorEenApplication {
+public class AllenVoorEenApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AllenVoorEenApplication.class, args);
+		System.setProperty("server.servlet.context-path", "/allenvooreen");
 	}
 }
