48e31f1e7a5247c45bbe3207f4f98cc99ef79f04
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2020-01-17 13:35:21 +0100

    back to the simple yml

--- src/test/java/com/wemakeitwork/allenvooreen/controller/newActivityControllerTest.java
+++ src/test/java/com/wemakeitwork/allenvooreen/controller/newActivityControllerTest.java
@@ -21,51 +21,51 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@ExtendWith(SpringExtension.class)
-//@WebMvcTest(controllers = ActivityController.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = ActivityController.class)
 class newActivityControllerTest {
-//
-//    @MockBean
-//    ActivityRepository activityRepository;
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    MemberDetailsService memberDetailsService;
-//
-//    @Test
-//    @WithMockUser(roles = "admin")
-//    public void shouldReturnNewActivity() throws Exception {
-//        mockMvc.perform(get("/activity/new"))
-//                .andExpect(status().isOk())
-//                .andExpect(forwardedUrl("/WEB-INF/jsp/newActivity.jsp"));
-//    }
-//
-//    @Test
-//    @WithMockUser(roles = "admin")
-//    public void shouldReturnNewActivityPost() throws Exception {
-//        String activityname = "test2";
-//        String activitycategory = "test4";
-//
-//        mockMvc.perform(post("/activity/new")
-//                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .param("activityName", activityname)
-//                .param("activityCategory", activitycategory)
-//                .flashAttr("activity", new Activity())
-//                .with(csrf())
-//        )
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(view().name("redirect:/activity/all"))
-//                .andExpect(redirectedUrl("/activity/all"));
-//
-//        ArgumentCaptor<Activity> formObjectArgument = forClass(Activity.class);
-//        verify(activityRepository, times(1)).save(formObjectArgument.capture());
-//        Mockito.verifyNoMoreInteractions(activityRepository);
-//
-//        Activity formObject = formObjectArgument.getValue();
-//
-//        Assertions.assertThat(formObject.getActivityName()).isEqualTo(activityname);
-//        Assertions.assertThat(formObject.getActivityCategory()).isEqualTo(activitycategory);
-//    }
+
+    @MockBean
+    ActivityRepository activityRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    MemberDetailsService memberDetailsService;
+
+    @Test
+    @WithMockUser(roles = "admin")
+    public void shouldReturnNewActivity() throws Exception {
+        mockMvc.perform(get("/activity/new"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/newActivity.jsp"));
+    }
+
+    @Test
+    @WithMockUser(roles = "admin")
+    public void shouldReturnNewActivityPost() throws Exception {
+        String activityname = "test2";
+        String activitycategory = "test4";
+
+        mockMvc.perform(post("/activity/new")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("activityName", activityname)
+                .param("activityCategory", activitycategory)
+                .flashAttr("activity", new Activity())
+                .with(csrf())
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/activity/all"))
+                .andExpect(redirectedUrl("/activity/all"));
+
+        ArgumentCaptor<Activity> formObjectArgument = forClass(Activity.class);
+        verify(activityRepository, times(1)).save(formObjectArgument.capture());
+        Mockito.verifyNoMoreInteractions(activityRepository);
+
+        Activity formObject = formObjectArgument.getValue();
+
+        Assertions.assertThat(formObject.getActivityName()).isEqualTo(activityname);
+        Assertions.assertThat(formObject.getActivityCategory()).isEqualTo(activitycategory);
+    }
 }
