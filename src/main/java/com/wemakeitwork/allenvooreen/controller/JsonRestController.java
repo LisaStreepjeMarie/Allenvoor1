b2aad7f40bb339c02b59677f2c55b81b87a32321
Harmen Kets <harmenkets@gmail.com>
2020-01-21 18:32:18 +0100

    Stream now directly returns eventList to ajax.

--- src/main/java/com/wemakeitwork/allenvooreen/controller/JsonRestController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/JsonRestController.java
@@ -1,7 +1,6 @@
 package com.wemakeitwork.allenvooreen.controller;
 
 import com.wemakeitwork.allenvooreen.model.Event;
-import com.wemakeitwork.allenvooreen.model.Team;
 import com.wemakeitwork.allenvooreen.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -9,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class JsonRestController {
@@ -23,23 +22,14 @@ public class JsonRestController {
     public List<Event> getJsonEventListPeriod(@PathVariable("team") final Integer teamId,
                                               @PathVariable("startdate") final long startDateEpoch,
                                               @PathVariable("enddate") final long endDateEpoch){
-        Team team = teamRepository.getOne(teamId);
 
-        Date startDate = new Date(startDateEpoch);
-        Date endDate = new Date(endDateEpoch);
-        List<Event> eventListPeriod = new ArrayList<>();
-        List<Event> fullEventList = team.getEventList();
-
-        // Fill eventListPeriod with events in the timeperiod fullcalendar is asking for
-        // Add the filtered events to eventListPeriod
-        fullEventList.stream()
+        // Return events that occur in the timeperiod the fullcalendar view is currently watching (from one team)
+        return teamRepository.getOne(teamId).getEventList().stream()
                 // Filter events that occur after the startdate (of fullcalendars view)
-                .filter(x -> x.getEventStartDate().after(startDate))
+                .filter(x -> x.getEventStartDate().after(new Date(startDateEpoch)))
                 // Filter events that occur before the enddate (of fullcalendars view)
-                .filter(x -> x.getEventEndDate().before(endDate))
-                // Add the filtered events to the eventListPeriod
-                .forEach(eventListPeriod::add);
-
-        return eventListPeriod;
+                .filter(x -> x.getEventEndDate().before(new Date(endDateEpoch)))
+                // Return the filtered events
+                .collect(Collectors.toList());
     }
 }
