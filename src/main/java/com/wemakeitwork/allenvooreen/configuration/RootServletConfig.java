768b2d77f77d4c6d35854b3c6d72f76464b59415
Harmen Kets <harmenkets@gmail.com>
2020-01-21 22:04:38 +0100

    Added RootServletConfig.java to redirect root to /allenvooreen So http://localhost:8080 now redirects to http://localhost:8080/allenvooreen

---
+++ src/main/java/com/wemakeitwork/allenvooreen/configuration/RootServletConfig.java
@@ -0,0 +1,52 @@
+package com.wemakeitwork.allenvooreen.configuration;
+
+import org.apache.catalina.Host;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+@Configuration
+public class RootServletConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory() {
+
+            @Override
+            protected void prepareContext(Host host, ServletContextInitializer[] initializers) {
+                super.prepareContext(host, initializers);
+                StandardContext child = new StandardContext();
+                child.addLifecycleListener(new Tomcat.FixContextListener());
+                child.setPath("");
+                ServletContainerInitializer initializer = getServletContextInitializer(getContextPath());
+                child.addServletContainerInitializer(initializer, Collections.emptySet());
+                child.setCrossContext(true);
+                host.addChild(child);
+            }
+        };
+    }
+
+    private ServletContainerInitializer getServletContextInitializer(String contextPath) {
+        return (c, context) -> {
+            Servlet servlet = new HttpServlet() {
+                @Override
+                protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.sendRedirect(contextPath);
+                }
+            };
+            context.addServlet("root", servlet).addMapping("/*");
+        };
+    }
+}
