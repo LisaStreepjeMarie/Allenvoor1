4098f4514126760df3190873d4e670aa3362f72d
Harmen Kets <harmenkets@gmail.com>
2020-01-24 10:04:44 +0100

    Refactored calendar.js and added comments

--- src/main/java/com/wemakeitwork/allenvooreen/controller/JsonRestController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/JsonRestController.java
@@ -27,7 +27,7 @@ public class JsonRestController {
     @Autowired
     EventRepository eventRepository;
 
-    @GetMapping("/calendar/json/{teamid}/{startdate}/{enddate}")
+    @GetMapping("/calendar/get/{teamid}/{startdate}/{enddate}")
     public List<Event> getJsonEventListPeriod(@PathVariable("teamid") final Integer teamId,
                                               @PathVariable("startdate") final long startDateEpoch,
                                               @PathVariable("enddate") final long endDateEpoch) {
@@ -42,8 +42,7 @@ public class JsonRestController {
                 .collect(Collectors.toList());
     }
 
-    //TODO works but the event isn't saved
-    @PostMapping("/calendar/saveEventFromPost")
+    @PostMapping("/calendar/new/event")
     public ResponseEntity<Object> addEvent(@RequestBody String json) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonNode = new ObjectMapper().readTree(json);
@@ -64,10 +63,23 @@ public class JsonRestController {
         return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/calendar/json/medications")
-    public List<Medication> getMedications() {
-        System.out.println("hello!!");
-        return teamRepository.getOne(1).getMedicationList();
+    @PostMapping("/calendar/change/eventdate")
+    public ResponseEntity<Object> changeEventDate(@RequestBody String json) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Event newDates = mapper.readValue(json, Event.class);
+
+        Event event = eventRepository.getOne(newDates.getEventId());
+        event.setEventStartDate(newDates.getEventStartDate());
+        event.setEventEndDate(newDates.getEventEndDate());
+        eventRepository.save(event);
+
+        ServiceResponse<Event> response = new ServiceResponse<Event>("success", newDates);
+        return new ResponseEntity<Object>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/calendar/get/medicationlist/{teamid}")
+    public List<Medication> getMedications(@PathVariable("teamid") final Integer teamId) {
+        return teamRepository.getOne(teamId).getMedicationList();
     }
 
 }
--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,9 +1,18 @@
-$(document).ready(function() {
-        var ctx = $('#contextPathHolder').attr('data-contextPath');
+// Define contextpath
+var ctx = $('#contextPathHolder').attr('data-contextPath');
 
+// Get csrf token (needed to post a json through spring boot security
+$.ajaxSetup({
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader('X-CSRF-TOKEN', $('#csrfToken').attr('data-csrfToken'));
+    }
+});
+
+// functions in this method will only be available after the document (jsp) is completely loaded
+$(document).ready(function() {
         hideAllModalInputFields();
 
-        <!-- this shows/hides the eventDone input field when the checkbox is toggled -->
+        // this shows/hides the eventDone input field when the checkbox is toggled
         $("#eventDone").change(function () {
             if(document.getElementById("eventDone").checked == true) {
                     $("#datetimepickerDone").show()
@@ -13,18 +22,19 @@ $(document).ready(function() {
             }
         });
 
-        <!-- below makes sure that the unwanted fields in the modal are hidden and calls the selection upon change -->
+        // This makes sure that the unwanted fields in the modal are hidden and calls the selection upon change
         $("#activityCategory").change(function () {
             hideAllModalInputFields();
             activitySelection();
         });
 
-        <!-- below cleans the modal upon closing -->
+        // Cleans the modal upon closing
         $('#modal-form').on("hide.bs.modal", function() {
             $('#modal-form').trigger("reset");
             hideAllModalInputFields();
         });
 
+        // Loads fullcalendar
         $('#calendar').fullCalendar({
             themeSystem: 'bootstrap4',
             timeZone: 'Europe/Amsterdam',
@@ -44,15 +54,15 @@ $(document).ready(function() {
             selectable: true,
             selectHelper: true,
 
-            <!-- This function is executed when an empty date/time is clicked -->
+            // This function is executed when an empty date/time is clicked
             select: function(start, end) {
 
-                <!-- placeholder positie voor getMedication functie, moet in de modal gevuld worden denk ik -->
+                // placeholder positie voor getMedication functie, moet in de modal gevuld worden denk ik
                 getMedication();
 
                 $(".fc-highlight").css("background", "purple");
 
-                $('#save-change-event').attr('onclick', "saveEvent()");
+                $('#save-change-event').attr('onclick', "saveNewEvent()");
 
                 $('.modal').find('#eventStartDate').val(start.format('DD-MM-YYYY H:mm'));
                 $('.modal').find('#eventEndDate').val(end.format('DD-MM-YYYY H:mm'));
@@ -65,11 +75,8 @@ $(document).ready(function() {
 
             <!-- This function is executed when an already planned event is clicked -->
             eventClick: function(event, element) {
-                <!-- pass eventId to a simple <a href> link: -->
-                <!-- $('#deleteEvent').attr('href', ctx + "/event/delete/" + event.id); -->
-
-                <!-- redefines the onclick action of the delete-event button, so that it will point the browser -->
-                <!-- to the /event/delete/{eventId}/{activityId} mapping -->
+                // redefines the onclick action of the delete-event button, so that it will point the browser -->
+                // to the /event/delete/{eventId}/{activityId} mapping
                 $('#delete-event').attr('onclick',"window.location='" + ctx + "/event/delete/" + event.id + "/" + event.activity.id + "'");
 
                 $("#eventId").val(event.id);
@@ -79,42 +86,88 @@ $(document).ready(function() {
                 $('.modal').find('#activityCategory').val(event.activity.category);
                 $('.modal').find('#eventName').val(event.title);
 
-                <!-- below shows the modal based on the event.activity.category -->
+                // Shows modal fields based on the event.activity.category
                 fillingTheModal();
                 showMedicationAmount(event, element);
 
                 $('.modal').find('#eventStartDate').val(event.start.format('DD-MM-YYYY H:mm'));
                 $('.modal').find('#eventEndDate').val(event.end.format('DD-MM-YYYY H:mm'));
 
-                <!-- redefines the modal (popup) buttons with the appropriate button text -->
+                // Redefines the modal (popup) buttons with the appropriate button text
                 document.getElementById("modal-title").innerHTML = "Wijzig of verwijder afspraak";
                 document.getElementById("save-change-event").innerHTML = "Wijzig afspraak";
 
-                <!-- shows the delete button on the (still hidden) modal -->
+                // Shows the delete button on the (still hidden) modal
                 $("#delete-event").show();
 
-                <!-- lastly, the modal (popup) is shown, which by now has been properly configured -->
+                // lastly, the modal (popup) is shown, which by now has been properly configured
                 $('.modal').modal('show');
             },
 
-            eventDragStop: function(event, info) {
-                console.log(event.title + " was dragged to start: " + event.start.format() + " and end: " + event.end.format());
+            // This function is executed when an event is dragged to another date
+            eventDrop: function(event, info) {
+                // Puts needed fullcalendar event.data attributes into an object to pass to the REST api
+                // (for resizing and dragging an event we only need to know: a. which event, and b. what are the new dates.
+                currentEvent = {
+                    id: event.id,
+                    start: event.start,
+                    end: event.end,
+                }
+
+                // Make AJAX Post to change eventdate
+                $.ajax({
+                    url: ctx + "/calendar/change/eventdate",
+                    method: "POST",
+                    contentType: "application/json; charset=UTF-8",
+                    data: JSON.stringify(currentEvent),
+                    dataType: 'json',
+                    async: true,
+                    success: function(result) {
+                            $('#calendar').fullCalendar('refetchEvents');
+                    },
+                    error : function(e) {
+                        alert("Error sending new event with AJAX!")
+                        console.log("ERROR: ", e);
+                    }
+                });
             },
 
+            // This function is executed when an event is resized
+            // TODO: this code (along with eventDrop) is a prime candidate for refactoring as they are duplicates
             eventResize: function(event, delta) {
-                console.log(event.title + " was resized to start: " + event.start.format() + " and end: " + event.end.format());
+                currentEvent = {
+                    id: event.id,
+                    start: event.start,
+                    end: event.end,
+                }
+
+                $.ajax({
+                    url: ctx + "/calendar/change/eventdate",
+                    method: "POST",
+                    contentType: "application/json; charset=UTF-8",
+                    data: JSON.stringify(currentEvent),
+                    dataType: 'json',
+                    async: true,
+                    success: function(result) {
+                            $('#calendar').fullCalendar('refetchEvents');
+                    },
+                    error: function(e) {
+                        alert("Error sending new event with AJAX!")
+                        console.log("ERROR: ", e);
+                    }
+                });
             },
 
             // Distinct event colors based on activity.category
             eventRender: function(event, element) {
                 if (typeof event.activity !== 'undefined') {
                     if( event.activity.category == "Medisch") {
-                        element.css('background-color', '#000');
+                        element.css('background-color', 'green');
                     }
                 }
             },
 
-            <!-- Remember last view on page reload. -->
+            // This function makes the browser remember the last view on a page reload. -->
             viewRender: function (view, element) {
                 localStorage.setItem("fcDefaultView", view.name);
             },
@@ -122,11 +175,11 @@ $(document).ready(function() {
 
             editable: true,
 
-            <!-- calendarData is a JSON string with all calendar events -->
+            // This function gets all calendar events within the view using an AJAX get.
             events: function(start, end, timezone, callback) {
                 $.ajax({
                     type:'GET',
-                    url: ctx + '/calendar/json/' + $('#teamId').attr('data-teamId') + '/' + start + '/' + end + '/',
+                    url: ctx + '/calendar/get/' + $('#teamId').attr('data-teamId') + '/' + start + '/' + end + '/',
                     dataType: 'json',
                     error: function (xhr, type, exception) { alert("Error: " + exception); },
                     success: function (response) {
@@ -146,10 +199,12 @@ $(document).ready(function() {
                 });
              },
 
-            eventLimit: true // allow "more" link when too many events
+            // If a date has many dates, show an 'extra' button to
+            eventLimit: true
         });
+        // End of fullcalendar configuration
 
-        <!-- These functions load the start, end & done date calendars (datetimepickers) in the modal (popup). -->
+        // These functions load the start, end & done date calendars (datetimepickers) in the modal (popup).
         $('#datetimepickerStart').datetimepicker({
             format: 'DD-MM-YYYY HH:mm'
         });
@@ -166,99 +221,93 @@ $(document).ready(function() {
             $('#datetimepickerStart').datetimepicker('maxDate', e.date);
         });
         $('#datetimepickerDone').datetimepicker();
+    }); // End of $(document).ready(function)
 
+// This function shows the correct modal form based on the activity selection
+function activitySelection() {
+    if ($("#activityCategory").val() === "Medisch") {
+        $("#eventNameDiv, #eventDateStartEndDiv, #medicationChoiceDiv, #takenMedicationDiv, #eventDatesDiv").show();
+    } else {
+        $("#eventNameDiv, #eventDateStartEndDiv, #eventDatesDiv, #eventCommentDiv").show();
+    }
+    $("#datetimepickerDone").hide();
+    $("#eventDoneDiv").css("display", "");
+}
+
+// This function hides all modal options
+function hideAllModalInputFields() {
+    $("#eventNameDiv, #eventCommentDiv, #medicationChoiceDiv, #eventDatesDiv, #takenMedicationDiv").css("display", "none");
+}
+
+function showMedicationAmount(event, element){
+    if ($('.modal').find('#activityCategory').val() == "Medisch")
+      $('.modal').find('#takenMedication').val(event.activity.takenmedication);
+}
+
+// This function fills the modal with event info if it exist
+function fillingTheModal() {
+    if ($('.modal').find('#activityCategory').val() == "Medisch") {
+        $("#eventNameDiv, #eventDateStartEndDiv, #medicationChoiceDiv, #takenMedicationDiv, #eventDatesDiv").show();
+    } else {
+        $("#eventNameDiv, #eventDateStartEndDiv, #eventDatesDiv, #eventCommentDiv").show();
+    }
+    $("#datetimepickerDone").hide();
+    $("#eventDoneDiv").css("display", "");
+}
+
+function saveNewEvent() {
+    // Fill the object currentEvent with values from input fields in the modal
+    currentEvent = {
+        title: document.getElementById("eventName").value,
+        start: moment(document.getElementById("eventStartDate").value, "DD-MM-YYYY H:mm").toDate(),
+        end: moment(document.getElementById("eventEndDate").value, "DD-MM-YYYY H:mm").toDate(),
+        description: document.getElementById("eventComment").value,
+        activity: {
+            name: document.getElementById("eventName").value,
+            category: document.getElementById("activityCategory").value,
+        },
+        team: {
+            id: parseInt($('#teamId').attr('data-teamId'), 10),
+        }
+    }
+
+    // Send the currentEvent object to the controller with an AJAX post
+    $.ajax({
+        url: ctx + "/calendar/new/event",
+        method: "POST",
+        contentType: "application/json; charset=UTF-8",
+        data: JSON.stringify(currentEvent),
+        dataType : 'json',
+        async: true,
+        success: function(result) {
+            // Reload events on calendar if new event is written to the database successfully
+            $('#calendar').fullCalendar('refetchEvents');
+        },
+        error : function(e) {
+            alert("Error sending new event with AJAX!")
+            console.log(currentEvent)
+            console.log("ERROR: ", e);
+        }
     });
+}
 
-<!-- below function shows the correct modal form based on the activity selection -->
-    function activitySelection() {
-        if ($("#activityCategory").val() === "Medisch") {
-            $("#eventNameDiv, #eventDateStartEndDiv, #medicationChoiceDiv, #takenMedicationDiv, #eventDatesDiv").show();
-        } else {
-            $("#eventNameDiv, #eventDateStartEndDiv, #eventDatesDiv, #eventCommentDiv").show();
-        }
-        $("#datetimepickerDone").hide();
-        $("#eventDoneDiv").css("display", "");
-    }
-
-<!-- below function hides all modal options -->
-    function hideAllModalInputFields() {
-        $("#eventNameDiv, #eventCommentDiv, #medicationChoiceDiv, #eventDatesDiv, #takenMedicationDiv").css("display", "none");
-    }
-
-    function showMedicationAmount(event, element){
-        if ($('.modal').find('#activityCategory').val() == "Medisch")
-          $('.modal').find('#takenMedication').val(event.activity.takenmedication);
-    }
-
-<!-- below function fills the modal with event info if it exist -->
-    function fillingTheModal() {
-        if ($('.modal').find('#activityCategory').val() == "Medisch") {
-            $("#eventNameDiv, #eventDateStartEndDiv, #medicationChoiceDiv, #takenMedicationDiv, #eventDatesDiv").show();
-        } else {
-            $("#eventNameDiv, #eventDateStartEndDiv, #eventDatesDiv, #eventCommentDiv").show();
-        }
-        $("#datetimepickerDone").hide();
-        $("#eventDoneDiv").css("display", "");
-    }
-
-
-    /* This function is not available from jsp if within $(document).ready(function() */
-    function saveEvent() {
-        $.ajaxSetup({
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader('X-CSRF-TOKEN', $('#csrfToken').attr('data-csrfToken'));
-            }
-        });
-
-        currentEvent = {
-            title: document.getElementById("eventName").value,
-            start: moment(document.getElementById("eventStartDate").value, "DD-MM-YYYY H:mm").toDate(),
-            end: moment(document.getElementById("eventEndDate").value, "DD-MM-YYYY H:mm").toDate(),
-            description: document.getElementById("eventComment").value,
-            activity: {
-                name: document.getElementById("eventName").value,
-                category: document.getElementById("activityCategory").value,
-            },
-            team: {
-                id: parseInt($('#teamId').attr('data-teamId'), 10),
-            }
-        }
-
+// This function gets a medicationlist, moet nog aangepast worden voor med.id ipv med naam -->
+function getMedication(){
         $.ajax({
-            url : "/allenvooreen/calendar/saveEventFromPost",
-            method : "POST",
-            contentType : "application/json; charset=UTF-8",
-            data : JSON.stringify(currentEvent),
-            dataType : 'json',
-            async : true,
-            success : function(result) {
-                $('#calendar').fullCalendar('refetchEvents');
-            },
-            error : function(e) {
-                alert("Error sending new event with AJAX!")
-                console.log(currentEvent)
-                console.log("ERROR: ", e);
-            }
-        });
-    }
-
-<!-- functie om de lijst te vullen met specifieke medicaties, moet nog aangepast worden voor med.id ipv med naam -->
-    function getMedication(){
-            $.ajax({
-                 type:'GET',
-                 url: '/allenvooreen/calendar/json/medications',
-                 dataType: 'json',
-                 success : function(result) {
-                         List = result
-                         $('#medicationChoice').empty();
-                         $('#medicationChoice').append('<option value="">Kies een medicatie</option>');
-                              for (i in List ) {
-                              $('#medicationChoice').append('<option value="' + List[i].medicationname + '">' + List[i].medicationname + '</option>');
-                     }
-                 },
-                 error : function(e) {
-                 alert("error Error ERROR!")
-                 console.log("ERROR: ", e);
-                  }
-          });
-    }
+             type:'GET',
+             url: ctx + "/calendar/get/medicationlist/" + $('#teamId').attr('data-teamId'),
+             dataType: 'json',
+             success : function(result) {
+                     List = result
+                     $('#medicationChoice').empty();
+                     $('#medicationChoice').append('<option value="">Kies een medicatie</option>');
+                          for (i in List ) {
+                          $('#medicationChoice').append('<option value="' + List[i].medicationname + '">' + List[i].medicationname + '</option>');
+                 }
+             },
+             error : function(e) {
+             alert("error Error ERROR!")
+             console.log("ERROR: ", e);
+              }
+      });
+}
