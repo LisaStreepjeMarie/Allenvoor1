e4d82ec0ec1bfab0365491df4ffc9ee7a4d97827
Harmen Kets <harmenkets@gmail.com>
2020-02-03 11:22:23 +0100

    Refactored principal usage (principal is now cast to member).

--- src/main/java/com/wemakeitwork/allenvooreen/controller/MedicationController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/MedicationController.java
@@ -1,12 +1,15 @@
 package com.wemakeitwork.allenvooreen.controller;
 
-import com.wemakeitwork.allenvooreen.model.*;
+import com.wemakeitwork.allenvooreen.model.Medication;
+import com.wemakeitwork.allenvooreen.model.Member;
+import com.wemakeitwork.allenvooreen.model.Team;
 import com.wemakeitwork.allenvooreen.repository.MedicationRepository;
 import com.wemakeitwork.allenvooreen.repository.MemberRepository;
 import com.wemakeitwork.allenvooreen.repository.TeamRepository;
 import com.wemakeitwork.allenvooreen.service.MedicationServiceInterface;
 import com.wemakeitwork.allenvooreen.validator.MedicationValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,8 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.security.Principal;
-import java.util.Optional;
 import java.util.Set;
 
 @Controller
@@ -49,22 +50,18 @@ public class MedicationController {
     }
 
     @GetMapping("/medication/{teamId}")
-    public String showMedication(@PathVariable("teamId") final Integer teamId, Model model, Principal principal) {
+    public String showMedication(@PathVariable("teamId") final Integer teamId, Model model) {
         Team team = teamRepository.getOne(teamId);
         httpSession.setAttribute("team", team);
 
-        Set<Team> teamList = null;
-        Optional<Member> member = memberRepository.findByMemberName(principal.getName());
-        if (member.isPresent()) {
-            teamList = member.get().getAllTeamsOfMemberSet();
-        }
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Set<Team> teamList = member.getAllTeamsOfMemberSet();
 
         Medication medication = new Medication();
         model.addAttribute("medicationList", team.getMedicationList());
 
         return "medicationOverview";
     }
-
 
     @GetMapping("/medication/delete/{medicationId}")
     public String deleteTeam(@PathVariable("medicationId") final Integer medicationId) {
--- src/main/java/com/wemakeitwork/allenvooreen/controller/MemberController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/MemberController.java
@@ -1,22 +1,22 @@
 package com.wemakeitwork.allenvooreen.controller;
 
 import com.wemakeitwork.allenvooreen.model.Member;
-import com.wemakeitwork.allenvooreen.model.Team;
 import com.wemakeitwork.allenvooreen.repository.MemberRepository;
 import com.wemakeitwork.allenvooreen.repository.TeamRepository;
 import com.wemakeitwork.allenvooreen.service.MemberServiceInterface;
 import com.wemakeitwork.allenvooreen.service.SecurityServiceInterface;
 import com.wemakeitwork.allenvooreen.validator.MemberValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
-import java.security.Principal;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 
 @Controller
 public class MemberController {
@@ -40,8 +40,9 @@ public class MemberController {
     private MemberValidator memberValidator;
 
     @GetMapping("member/current")
-    protected String showMember(Model model, Principal principal){
-        model.addAttribute("currentmember", memberRepository.findByMemberName(principal.getName()).orElse(new Member()));
+    protected String showMember(Model model){
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("currentmember", member);
         return "memberProfile";
     }
 
@@ -52,12 +53,8 @@ public class MemberController {
     }
 
     @GetMapping("/member/select/{memberId}")
-    protected String showMemberData(Model model, Principal principal) {
-        model.addAttribute("member", new Member());
-
-        Optional<Member> memberOpt = memberRepository.findByMemberName(principal.getName());
-        Member member;
-        member = memberOpt.orElseGet(Member::new);
+    protected String showMemberData(Model model) {
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("member", member);
         return "changeMember";
     }
@@ -70,14 +67,14 @@ public class MemberController {
     }
 
     @GetMapping("/member/delete")
-    public String deleteMember(Principal principal) {
-        Optional<Member> member = memberRepository.findByMemberName(principal.getName());
-        member.ifPresent(value -> memberRepository.delete(value));
+    public String deleteMember() {
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        memberRepository.delete(member);
         return "confirmLogout";
     }
 
     @PostMapping("/member/new")
-    protected String saveOrUpdateMember(@ModelAttribute("member") @Valid Member member, BindingResult result) {
+    protected String newMember(@ModelAttribute("member") @Valid Member member, BindingResult result) {
         System.out.println("is er output? " + member.getMemberName());
         memberValidator.validate(member, result);
         if (result.hasErrors()) {
@@ -93,16 +90,14 @@ public class MemberController {
     }
 
     @PostMapping("/member/change")
-    protected String saveOrUpdateMember(@ModelAttribute("currentmember") Member newNameMember, BindingResult result, Principal principal) {
+    protected String changeMember(@ModelAttribute("currentmember") Member newNameMember, BindingResult result) {
         if (result.hasErrors()) {
             return "currentMember";
         } else {
-            Optional<Member> originalMember = memberRepository.findByMemberName(principal.getName());
-            if (originalMember.isPresent()){
-                newNameMember.setPassword(originalMember.get().getPassword());
-                newNameMember.setMemberId(originalMember.get().getMemberId());
-                newNameMember.setRol(originalMember.get().getRol());
-            }
+            Member originalMember = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            newNameMember.setPassword(originalMember.getPassword());
+            newNameMember.setMemberId(originalMember.getMemberId());
+            newNameMember.setRol(originalMember.getRol());
             memberRepository.save(newNameMember);
             return "redirect:/logout";
         }
