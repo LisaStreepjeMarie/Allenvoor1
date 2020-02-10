fc9dcbb27d8b669569287164efc6b8df1995a78d
LisaStreepjeMarie <LisaStreepjeMarie@gmail.com>
2020-02-10 12:31:13 +0100

    added lay out stuff

--- src/main/resources/static/js/events.js
+++ src/main/resources/static/js/events.js
@@ -24,9 +24,9 @@ function saveEvent(eventId, activityId) {
             end: moment(document.getElementById("eventEndDate").value, "DD-MM-YYYY H:mm").toDate(),
             comment: document.getElementById("eventComment").value,
             donedate: moment(document.getElementById("eventDoneDate").value, "DD-MM-YYYY H:mm").toDate(),
-            doneByMember: {
-                id: document.getElementById("doneByMember").value,
-            },
+//            doneByMember: {
+//                id: document.getElementById("doneByMember").value,
+//            },
             activity: {
                 type: activityType,
                 activityId: document.getElementById("activityId").value,
@@ -75,8 +75,8 @@ function getMedication(event){
                  for (i in List ) {
                     $('#medicationChoice').append('<option value="' + List[i].id + '">' + List[i].name + '</option>');
                  }
-                 document.getElementById("medicationChoice").value = event.activity.medication.id;
-                 $('.modal').find('#takenMedication').val(event.activity.takenmedication);
+//                 document.getElementById("medicationChoice").value = event.activity.medication.id;
+//                 $('.modal').find('#takenMedication').val(event.activity.takenmedication);
              },
              error : function(e) {
              alert("getMedication() error")
