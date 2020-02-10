0372baa4bc35bd08bd9b83744d35d6730daa9638
Luisella <luisella@xs4all.nl>
2020-02-10 17:01:08 +0100

    setup testing CalendarController

--- src/test/java/com/wemakeitwork/allenvooreen/model/EventTest.java
+++ src/test/java/com/wemakeitwork/allenvooreen/model/EventTest.java
@@ -47,6 +47,42 @@ class EventTest {
         Assertions.assertThat(testEvent.getEventId()).isEqualTo(2);
     }
 
+    @Test
+    void getEventInterval() {
+        //arrange
+        testEvent.setEventInterval("day");
+
+        //assert
+        Assertions.assertThat(testEvent.getEventInterval()).isEqualTo("day");
+    }
+
+    @Test
+    void setEventEventInterval() {
+        //arrange
+        testEvent.setEventInterval("week");
+
+        //assert
+        Assertions.assertThat(testEvent.getEventInterval()).isEqualTo("week");
+    }
+
+    @Test
+    void getEventMaxNumber() {
+        //arrange
+        testEvent.setEventMaxNumber(5);
+
+        //assert
+        Assertions.assertThat(testEvent.getEventMaxNumber()).isEqualTo(5);
+    }
+
+    @Test
+    void setEventMaxNumber() {
+        //arrange
+        testEvent.setEventMaxNumber(5);
+
+        //assert
+        Assertions.assertThat(testEvent.getEventMaxNumber()).isEqualTo(5);
+    }
+
 /*
     @Test
     void getActivity() {
