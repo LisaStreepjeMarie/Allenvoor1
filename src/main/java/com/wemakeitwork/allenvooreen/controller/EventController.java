98c342d6bf5fbfe0ebe4281aaa2c9acc46bdbcec
Harmen Kets <harmenkets@gmail.com>
2020-02-11 20:06:45 +0100

    added (temporary) subscriptionList.jsp (temporarily) disabled email confirmation mailsender and set member to enabled by default for testing purposes.

--- src/main/java/com/wemakeitwork/allenvooreen/controller/EventController.java
+++ src/main/java/com/wemakeitwork/allenvooreen/controller/EventController.java
@@ -1,26 +1,22 @@
 package com.wemakeitwork.allenvooreen.controller;
 
 import com.wemakeitwork.allenvooreen.model.Event;
+import com.wemakeitwork.allenvooreen.model.EventSubscription;
 import com.wemakeitwork.allenvooreen.model.MedicationActivity;
 import com.wemakeitwork.allenvooreen.model.Team;
-import com.wemakeitwork.allenvooreen.repository.ActivityRepository;
-import com.wemakeitwork.allenvooreen.repository.EventRepository;
-import com.wemakeitwork.allenvooreen.repository.MedicationRepository;
-import com.wemakeitwork.allenvooreen.repository.TeamRepository;
+import com.wemakeitwork.allenvooreen.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Set;
 
 @Controller
 public class EventController {
@@ -35,6 +31,9 @@ public class EventController {
 
     @Autowired
     TeamRepository teamRepository;
+
+    @Autowired
+    EventSubscriptionRepository eventSubscriptionRepository;
 
     @Autowired
     private HttpSession httpSession;
@@ -88,4 +87,18 @@ public class EventController {
             return "redirect:/calendar/" + team.getTeamId();
         }
     }
+
+    @GetMapping("/event/{eventid}/subscriptionlist")
+    protected String showSubscriptionList(@PathVariable("eventid") final Integer eventId, Model model) {
+        Set<EventSubscription> eventSubscriptionSet = eventSubscriptionRepository.findByEventEventId(eventId);
+        if (eventSubscriptionSet.size() != 0) {
+            for (EventSubscription eventSubscription : eventSubscriptionSet) {
+                System.out.println(eventSubscription.getMember());
+            }
+        }
+        model.addAttribute("eventSubscriptionSet", eventSubscriptionSet);
+        return "subscriptionList";
+    }
+
+
 }
--- src/main/java/com/wemakeitwork/allenvooreen/repository/EventSubscriptionRepository.java
+++ src/main/java/com/wemakeitwork/allenvooreen/repository/EventSubscriptionRepository.java
@@ -3,6 +3,9 @@ package com.wemakeitwork.allenvooreen.repository;
 import com.wemakeitwork.allenvooreen.model.EventSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface EventSubscriptionRepository extends JpaRepository<EventSubscription, Integer> {
 
+    Set<EventSubscription> findByEventEventId(Integer eventId);
 }
